dddbcedb9224d762a7a112802f163ae37493721b
Soo Hian <shonendumm@gmail.com>
2020-11-07 10:51:50 +0800

    callback after toggle

--- code.js
+++ code.js
@@ -5,12 +5,12 @@ $(document).ready(function() {
     });
 
     $("button").click(function() {
-        $("p").toggle(1000); // toggle is to hide or show
+        $("p").toggle(1000, function() { alert("callback") }); // toggle is to hide or show
     })
 
-    $("#alert_here").mousedown(function() {
-        alert("hello")
-    })
+    // $("#alert_here").mousedown(function() {
+    //     alert("hello")
+    // })
 
     $("input").on({
         focus: function() {
