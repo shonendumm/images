4e678ca171cec0e180ebd722a1f0d63e9b25b13f
Soo Hian <shonendumm@gmail.com>
2020-11-07 12:21:12 +0800

    remain pics only

--- page01.js
+++ page01.js
@@ -16,38 +16,4 @@ $(document).ready(function() {
 
 
 
-    // previous code stuff on buttons and getting/setting html attributes
-    $("#but1").click(function() {
-
-        var x = $("a").attr("href");
-        alert(x);
-
-    });
-
-    $("#but2").click(function() {
-        $("#p01").text("changed")
-
-    })
-
-
-    $("#but3").click(function() {
-        $("input").val("")
-
-    })
-
-    // reminder on how to do multiple event handlers
-
-    $("input").on({
-
-        focus: function() {
-            $(this).css("background-color", "lightgrey")
-        }, // important to put comma
-
-        blur: function() {
-            $(this).css("background-color", "white")
-        }
-
-    })
-
-
 });
